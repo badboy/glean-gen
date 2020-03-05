8bdc4490bb829bfd8df0bde4951825b3f6ef5fca
Jan-Erik Rediger <janerik@fnordig.de>
2020-03-05 14:26:42 +0100

    Add IPC communication

--- glean/src/metrics.rs
+++ glean/src/metrics.rs
@@ -1,13 +1,58 @@
 #[derive(Debug)]
-pub struct StringMetric;
+pub enum StringMetric {
+    Parent(StringMetricImpl),
+    Child(StringMetricIpc),
+}
+
+#[derive(Debug)]
+pub struct StringMetricImpl;
+#[derive(Debug)]
+pub struct StringMetricIpc;
+
+extern "C" {
+    fn is_parent_proc() -> bool;
+    fn ipc_send_buffer(buffer: Vec<u8>);
+}
 
 impl StringMetric {
+    pub fn new() -> Self {
+        if unsafe {is_parent_proc() } {
+            StringMetric::Parent(StringMetricImpl::new())
+        } else {
+            StringMetric::Child(StringMetricIpc::new())
+        }
+    }
+
+    pub fn set<S: Into<String>>(&self, value: S) {
+        let value = value.into();
+        match self {
+            StringMetric::Parent(p) => p.set(value),
+            StringMetric::Child(c) => c.set(value),
+        }
+    }
+}
+
+
+impl StringMetricImpl {
     pub fn new() -> Self {
         Self
     }
 
-    pub fn set<S: Into<String>>(&self, value: S) {
-        println!("Setting string metric to: {:?}", value.into());
+    pub fn set(&self, value: String) {
+        println!("Setting string metric to: {:?}", value);
     }
 }
 
+impl StringMetricIpc {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn set(&self, value: String) {
+        println!("IPC-sending string metric with value: {:?}", value);
+        unsafe {
+            let buffer = bincode::serialize(&value).unwrap();
+            ipc_send_buffer(buffer);
+        }
+    }
+}
--- user/src/main.rs
+++ user/src/main.rs
@@ -1,5 +1,57 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+
+use serde::{Serialize, Deserialize};
+use procspawn::{self, spawn};
+use ipc_channel::ipc::{self, IpcSender};
+
 mod metrics;
 
+static mut IS_PARENT_PROCESS: AtomicBool = AtomicBool::new(true);
+static mut IPC_SENDER: Option<IpcSender<Command>> = None;
+
+#[no_mangle]
+extern "C" fn is_parent_proc() -> bool {
+    // safe, it's an atomic
+    unsafe {
+        IS_PARENT_PROCESS.load(Ordering::SeqCst)
+    }
+}
+
+#[no_mangle]
+extern "C" fn ipc_send_buffer(buffer: Vec<u8>) {
+    unsafe {
+        match IPC_SENDER {
+            Some(ref sender) => sender.send(Command::Bytes(buffer)).unwrap(),
+            None => {}
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Bytes(Vec<u8>)
+}
+
 fn main() {
+    procspawn::init();
+
+    let (tx, rx) = ipc::channel::<Command>().unwrap();
+
+    let child_handle = spawn(tx, |tx| {
+        unsafe {
+            IS_PARENT_PROCESS.store(false, Ordering::SeqCst);
+            IPC_SENDER = Some(tx);
+        }
+
+        println!("Started child process.");
+        metrics::used::os.set("Windows");
+    });
+
     metrics::used::os.set("Windows");
+
+    let Command::Bytes(buffer) = rx.recv().unwrap();
+    let s: String = bincode::deserialize(&buffer).unwrap();
+    println!("received string: {:?}", s);
+
+    child_handle.join().unwrap();
 }
